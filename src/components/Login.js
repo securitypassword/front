6f32f695b277199fa10a5b4c1217230f3c70f0b8
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-25 19:33:06 -0600

    /passwords

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
 
     const navigate = useNavigate();
     const location = useLocation();
-    const from = location.state?.from?.pathname || "/newpassword";
+    const from = location.state?.from?.pathname || "/password";
 
     const userRef = useRef();
     const errRef = useRef();
